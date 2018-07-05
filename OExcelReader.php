3af140455bc074f92724f00882b50fac7b81df8c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-05 09:44:09 +0700

    change copyright info

--- OExcelReader.php
+++ OExcelReader.php
@@ -8,7 +8,7 @@
  *
  * @modify Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
- * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @copyright Copyright (c) 2017 Ommu Platform (www.ommu.co)
  * @created date 25 September 2017, 05:24 WIB
  * @link https://github.com/ommu/ommu-excel-reader
  *
